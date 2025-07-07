82853c0de089c1c039f5f7b49a2bcf66c423934a
Mansitha <iammansitha@gmail.com>
2025-07-07 16:53:47 +0530

    data validation part done

--- src/MLOps/config/configuration.py
+++ src/MLOps/config/configuration.py
@@ -1,7 +1,8 @@
 from ..constants import *
 from ..utils.common import read_yaml, create_directories
-from ..entity.config_entity import (DataIngestionConfig)
+from ..entity.config_entity import DataIngestionConfig, DataValidationConfig
 class ConfigurationManager:
+
     def __init__(
         self,
         config_filepath = CONFIG_FILE_PATH,
@@ -30,3 +31,18 @@ class ConfigurationManager:
 
         return data_ingestion_config
     
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema,
+        )
+
+        return data_validation_config
